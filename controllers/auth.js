c18fd04134135ca30dad7eaa1cd5b64c95b8dab3
Nirmalya Nayak <n.nayak263@gmail.com>
2022-12-22 19:56:59 +0530

    jwt integration

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const User = require('../models/user')
 
@@ -61,6 +62,20 @@ exports.login = (req, res, next) => {
         error.statusCode = 401
         throw error
       }
+      const token = jwt.sign(
+        {
+          email: loadedUser.email,
+          userId: loadedUser._id.toString(),
+        },
+        'whythiskolaveridi',
+        {
+          expiresIn: '1h',
+        },
+      )
+      res.status(200).json({
+        token: token,
+        userId: loadedUser._id.toString(),
+      })
     })
     .catch((err) => {
       setStatusCode500(err)
