be422702464d133cbc5369a24d0d2fa1273066b5
Nirmalya Nayak <n.nayak263@gmail.com>
2022-12-22 19:26:07 +0530

    updated user model and finised signup method

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,13 @@
 const { validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs')
 
 const User = require('../models/user')
+
+const setStatusCode500 = (err) => {
+  if (!err.statusCode) {
+    err.statusCode = 500
+  }
+}
 
 exports.signup = (req, res, next) => {
   const errors = validationResult(req)
@@ -11,4 +18,24 @@ exports.signup = (req, res, next) => {
     throw error
   }
   const { email, name, password } = req.body
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPassword) => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        name: name,
+      })
+      return user.save()
+    })
+    .then((result) => {
+      res.status(201).json({
+        message: 'New User Created!',
+        userId: result._id,
+      })
+    })
+    .catch((err) => {
+      setStatusCode500(err)
+      next(err)
+    })
 }
