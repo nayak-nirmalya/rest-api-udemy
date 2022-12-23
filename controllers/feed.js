b6b4ca24f9aab539d056032a1f5f294e4d75a268
Nirmalya Nayak <n.nayak263@gmail.com>
2022-12-23 13:40:20 +0530

    updated deletePost to emit 'delete' event

--- controllers/feed.js
+++ controllers/feed.js
@@ -171,6 +171,10 @@ exports.deletePost = async (req, res, next) => {
     user.posts.pull(postId)
     await user.save()
 
+    io.getIO().emit('posts', {
+      action: 'delete',
+      post: postId,
+    })
     console.log('DELETED!')
     res.status(200).json({
       message: 'Deleted Post!',
